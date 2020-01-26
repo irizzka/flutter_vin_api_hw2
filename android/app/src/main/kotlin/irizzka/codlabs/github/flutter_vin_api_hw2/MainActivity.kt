327a4d82ef294557be5b05c5fbeb8a7d8abc6514
irizzka <irizzavfx@gmail.com>
2020-01-26 23:03:14 +0200

    Grid hw2

---
+++ android/app/src/main/kotlin/irizzka/codlabs/github/flutter_vin_api_hw2/MainActivity.kt
@@ -0,0 +1,13 @@
+package irizzka.codlabs.github.flutter_vin_api_hw2
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
